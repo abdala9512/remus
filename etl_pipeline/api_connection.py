ed8eb044556eda1847fc2a7cdd91722084989cd9
Miguel Arquez Abdala <42754537+abdala9512@users.noreply.github.com>
2020-07-14 14:09:02 -0500

    API Class initialization

--- etl_pipeline/api_connection.py
+++ etl_pipeline/api_connection.py
@@ -1,3 +1,16 @@
+"""Spotify API connection"""
 
+import requests
+import json
+import base64
+
+
+SPOTIFY_ROOT_URL = "https://api.spotify.com"
 class ApiConnection:
-    pass
+    
+    def __init__(self, api='spotify'):
+        self.api = api
+
+    def spotify_connection(self, user):
+        pass
+    
