d23da5a433009b4e671ded30087691fb3c80743f
Miguel Arquez Abdala <42754537+abdala9512@users.noreply.github.com>
2020-08-08 11:14:00 -0500

    Modularized API connection, query modules

--- remus/etl_pipeline/api_connection.py
+++ remus/etl_pipeline/api_connection.py
@@ -17,7 +17,7 @@ class SpotifyAPIConnection:
     def access_token(self):
         return self._access_token
 
-    def get_token(self):
+    def auth(self):
         client_creds = f"{self.client_id}:{self.secret}"
         client_creds64 =  base64.b64encode(client_creds.encode())
 
@@ -34,3 +34,17 @@ class SpotifyAPIConnection:
             return False
         self._access_token =  r.json()['access_token']
         return True
+    
+    def get_access_token(self):
+        auth_done = self.auth()
+        if not auth_done:
+            raise Exception("Authentication failed")
+        token = self._access_token
+        return token
+
+    def get_resource_header(self):
+        access_token = self.get_access_token()
+        headers = {
+            "Authorization": f"Bearer {self.get_access_token()}"
+        }
+        return headers
--- remus/etl_pipeline/spotify_requests.py
+++ remus/etl_pipeline/spotify_requests.py
@@ -15,25 +15,45 @@ class SpotifyRequest(SpotifyAPIConnection):
         self.secret_id = secret_id
  
 
-    def search(self, query, type):
-        """[summary]
+    def search(self, query, search_type):
+        """Search for a keyword in Spotify 
 
         Args:
-            query ([String]): it could be "track", "album"
-            type ([type]): [description]
+            query (String): The query we want to look up
+            search_type (String): it could be "track", "album"
         """
-        header = {
-            "Authorization": f"Bearer {self.access_token}"
-        }
+
+        header = self.get_resource_header()
 
         endpoint = SpotifyRequest.BASE_ENDPOINT + "search"
-        data = urlencode({"q": query, "type": type})
+        data = urlencode({"q": query, "type": search_type.ower()})
 
         lookup_URL = f"{endpoint}?{data}"
 
         r = requests.get(lookup_URL, headers=header)
 
+        if r.status_code not in range(200, 299):
+            return {}
         return r.json()
+
+        def get_resource(self, lookup_id, resource_type='albums'):
+            endpoint = f"{SpotifyRequest.BASE_ENDPOINT}{resource_type}/{_id}"
+            header = self.header_resource()
+            r = requests.get(endpoint, headers=header)
+            if status_code not in range(200, 299):
+                return {}
+            return r.json()
+
+        def get_album(self, _id):
+            return self.get_resource(_id, resource_type='albums')
+
+        def get_artist(self, _id):
+            return self.get_resource(_id, resource_type='artists')
+
+        def get_track(self, _id)
+            return self.get_resource(_id, resource_type='track')
+
+        
 
 
 
