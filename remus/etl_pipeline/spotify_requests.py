42964db473d212994ae2980e5fee9d553454dabf
Miguel Arquez Abdala <42754537+abdala9512@users.noreply.github.com>
2020-08-08 12:16:48 -0500

    Search improvements

--- remus/etl_pipeline/spotify_requests.py
+++ remus/etl_pipeline/spotify_requests.py
@@ -15,7 +15,7 @@ class SpotifyRequest(SpotifyAPIConnection):
         self.secret_id = secret_id
  
 
-    def search(self, query, search_type):
+    def base_search(self, query_params):
         """Search for a keyword in Spotify 
 
         Args:
@@ -24,34 +24,39 @@ class SpotifyRequest(SpotifyAPIConnection):
         """
 
         header = self.get_resource_header()
-
         endpoint = SpotifyRequest.BASE_ENDPOINT + "search"
-        data = urlencode({"q": query, "type": search_type.ower()})
-
-        lookup_URL = f"{endpoint}?{data}"
-
+        lookup_URL = f"{endpoint}?{query_params}"
         r = requests.get(lookup_URL, headers=header)
 
         if r.status_code not in range(200, 299):
             return {}
         return r.json()
 
-        def get_resource(self, lookup_id, resource_type='albums'):
-            endpoint = f"{SpotifyRequest.BASE_ENDPOINT}{resource_type}/{_id}"
-            header = self.header_resource()
-            r = requests.get(endpoint, headers=header)
-            if status_code not in range(200, 299):
-                return {}
-            return r.json()
+    def search(self, query=None, search_type='artist'):
+        if query is None:
+            raise Exception("Query required:")
+        if isinstance(query, dict):
+            query = " ".join([f"{k}:{v}" for k,v in query.items()])
+        query_params = urlencode({"q": query, "type": search_type.lower()})
+        return self.base_search(query_params)
 
-        def get_album(self, _id):
-            return self.get_resource(_id, resource_type='albums')
 
-        def get_artist(self, _id):
-            return self.get_resource(_id, resource_type='artists')
+    def get_resource(self, lookup_id, resource_type='albums'):
+        endpoint = f"{SpotifyRequest.BASE_ENDPOINT}{resource_type}/{_id}"
+        header = self.header_resource()
+        r = requests.get(endpoint, headers=header)
+        if status_code not in range(200, 299):
+            return {}
+        return r.json()
 
-        def get_track(self, _id)
-            return self.get_resource(_id, resource_type='track')
+    def get_album(self, _id):
+        return self.get_resource(_id, resource_type='albums')
+
+    def get_artist(self, _id):
+        return self.get_resource(_id, resource_type='artists')
+
+    def get_track(self, _id):
+        return self.get_resource(_id, resource_type='track')
 
         
 
