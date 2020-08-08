687e844118dc2f5b54a3ad31bd60b4a1f08d1623
Miguel Arquez Abdala <42754537+abdala9512@users.noreply.github.com>
2020-08-07 20:41:27 -0500

    Spotify Base Class

--- remus/etl_pipeline/api_connection.py
+++ remus/etl_pipeline/api_connection.py
@@ -4,15 +4,30 @@ import requests
 import json
 import base64
 
+class SpotifyAPIConnection:
 
-SPOTIFY_ROOT_URL = "https://api.spotify.com"
-class ApiConnection:
-    
-    def __init__(self, api='spotify'):
-        self.api = api
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
-    def spotify_connection(self, user):
-        if self.api == 'spotify':
-            pass
-        else:
-            raise ConnectionError("Change your API connection to Spotify!")
+
+    def __init__(self, client_id, secret): 
+        self.client_id = client_id
+        self.secret = secret
+        self.access_token = None
+
+    def get_token(self):
+        client_creds = f"{self.client_id}:{self.secret}"
+        client_creds64 =  base64.b64encode(client_creds.encode())
+
+        token_data = {
+            'grant_type': 'client_credentials'
+        }
+
+        token_header = {
+            'Authorization': f"Basic {client_creds64.decode()}"
+        }
+
+        r = requests.post(TOKEN_URL, data=token_data, headers=token_header)
+        if r.status_code not in range(200,299)
+            return False
+        self.access_token =  r.json()['access_token']
+        return True
